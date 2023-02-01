b3cd43cdf040a99dd80efd99d3f365fe45c0726b
Sudhanshu Thakur <122377381+thesrt@users.noreply.github.com>
2023-02-01 14:46:00 +0530

    Add files via upload

---
+++ CF.java
@@ -0,0 +1,18 @@
+package CipherSchoolCourse2;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CF {
+	public static void main(String[] args) {
+		ArrayList<String> states = new ArrayList<String>();
+		
+		states.add("California");
+		states.add("Alabama");
+		states.add("Alaska");
+		states.add("Arkansas");
+		states.add("LosAngelas");
+		
+		System.out.println("ArrayList: "+states);
+	}
+	
+}
